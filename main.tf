379eadbd5e15affb947a14f8333ecb39486443d8
zhengjianhui <815168733@qq.com>
2022-09-01 07:39:10 +0800

    Initial commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region  = var.region
+  profile = var.profile
+}
+
+module "eks-jx" {
+  source               = "github.com/jenkins-x/terraform-aws-eks-jx?ref=v1.19.0"
+  cluster_version      = var.cluster_version
+  cluster_name         = var.cluster_name
+  region               = var.region
+  vault_user           = var.vault_user
+  is_jx2               = false
+  jx_git_url           = var.jx_git_url
+  jx_bot_username      = var.jx_bot_username
+  jx_bot_token         = var.jx_bot_token
+  force_destroy        = var.force_destroy
+  nginx_chart_version  = var.nginx_chart_version
+  install_kuberhealthy = var.install_kuberhealthy
+}
